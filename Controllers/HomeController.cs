8c8f1ab0fba0128cc375793f327059d4282ea158
LAPTOP-MEDEA0QO\werne <wernervstr@hotmail.com>
2018-12-17 11:41:55 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HairCity.Models;
+
+namespace HairCity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aanbod()
+        {
+            return View("aanbod");
+        }
+
+        public IActionResult Tarieven()
+        {
+            return View("tarieven");
+        }
+
+        public IActionResult Afspraak()
+        {
+            return View("afspraak");
+        }
+
+        public IActionResult Praktisch()
+        {
+            return View("praktisch");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
